9a9221617fd9779c0836c6e171c99de04d7facfe
malika-k <mkrkodial@gmail.com>
2024-12-30 14:42:21 -0600

    Set up prettier and eslint. Format all relevant files to followr rules for consistency.

--- codesign/app/(tabs)/profile.tsx
+++ codesign/app/(tabs)/profile.tsx
@@ -1,19 +1,19 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native';
 
-import { ThemedText } from "@/components/ThemedText";
-import { ThemedView } from "@/components/ThemedView";
+import { ThemedText } from '@/components/ThemedText';
+import { ThemedView } from '@/components/ThemedView';
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flex: 1,
-    padding: 16,
-  },
+	titleContainer: {
+		flex: 1,
+		padding: 16
+	}
 });
 
 export default function TabTwoScreen() {
-  return (
-    <ThemedView style={styles.titleContainer}>
-      <ThemedText type="title">Profile</ThemedText>
-    </ThemedView>
-  );
+	return (
+		<ThemedView style={styles.titleContainer}>
+			<ThemedText type="title">Profile</ThemedText>
+		</ThemedView>
+	);
 }
--- codesign/metro.config.js
+++ codesign/metro.config.js
@@ -2,7 +2,7 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname, {
-  // Additional features...
+	// Additional features...
 });
 
 module.exports = config;

2d2bcbfd664eba6e1cb5244d89a425223b30d4e1
malika-k <mkrkodial@gmail.com>
2024-12-31 13:26:51 -0600

    Add style utils

---
+++ codesign/constants/styles/Typography.ts
@@ -0,0 +1,18 @@
+
+export const Typography = {
+  textCenter: {
+    textAlign: "center",
+  },
+  textLeft: {
+    textAlign: "left",
+  },
+  textRight: {
+    textAlign: "right",
+  },
+  underline: {
+    textDecorationLine: "underline",
+  },
+  textUppercase: {
+    textTransform: "uppercase",
+  }
+}

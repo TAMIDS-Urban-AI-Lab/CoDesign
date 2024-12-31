3695a84656bf6b4727e11c6fee1786842d72cf3b
malika-k <mkrkodial@gmail.com>
2024-12-31 15:39:05 -0600

    Extract useFonts hook and load new fonts

--- codesign/app/_layout.tsx
+++ codesign/app/_layout.tsx
@@ -3,7 +3,6 @@ import {
   DefaultTheme,
   ThemeProvider
 } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
 import * as SplashScreen from 'expo-splash-screen';
 import { StatusBar } from 'expo-status-bar';
@@ -11,15 +10,14 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { useFonts } from '@/hooks/useFonts';
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
   const colorScheme = useColorScheme();
-  const [loaded] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
-  });
+  const [loaded] = useFonts();
 
   useEffect(() => {
     if (loaded) {

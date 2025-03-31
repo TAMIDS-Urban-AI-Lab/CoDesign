ff33d663c38b868a778433807a480c7f12ed650d
malika-k <mkrkodial@gmail.com>
2025-03-31 12:38:47 -0500

    Add additional tests for theme color

--- codesign/hooks/__tests__/useThemeColor-test.tsx
+++ codesign/hooks/__tests__/useThemeColor-test.tsx
@@ -1,5 +1,6 @@
 import { useThemeColor } from '@/hooks/useThemeColor';
 import * as colorScheme from '@/hooks/useColorScheme';
+import { tamuColors } from '@/constants/Colors';
 
 describe('useThemeColor()', () => {
   beforeAll(() => {
@@ -18,9 +19,26 @@ describe('useThemeColor()', () => {
 
       expect(useThemeColor(props, colorName)).toBe(props.light);
     });
+
+    test('selects dark color', () => {
+      (colorScheme.useColorScheme as jest.Mock).mockReturnValue('dark');
+      const props = { light: '#AAA', dark: '#BBB' };
+      const colorName = 'text';
+
+      expect(useThemeColor(props, colorName)).toBe(props.dark);
+    });
   });
 
   describe('when no props argument is provided', () => {
+    test('selects default light color', () => {
+      (colorScheme.useColorScheme as jest.Mock).mockReturnValue('light');
+
+      const props = {};
+      const colorName = 'background';
+
+      expect(useThemeColor(props, colorName)).toBe(tamuColors.white);
+    });
+
     test('selects default dark color', () => {
       (colorScheme.useColorScheme as jest.Mock).mockReturnValue('dark');
 

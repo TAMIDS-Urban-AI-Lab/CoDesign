f8b5b2e70f696b7b339a28d624b28a2c35efa808
malika-k <mkrkodial@gmail.com>
2025-04-17 15:06:20 -0500

    Use new test helpers to check if report renders validation errors

--- codesign/components/__tests__/report/ReportForm-test.tsx
+++ codesign/components/__tests__/report/ReportForm-test.tsx
@@ -6,26 +6,23 @@ import { ALBRITTON_BELL_TOWER } from '@/constants/map/Coordinates';
 import { mockGetReportByBoundarySuccess } from '@/mocks/api/mockGetReportByBoundary';
 import { mockMapbox } from '@/mocks/mockMapbox';
 import { mockReactHookForm } from '@/mocks/mockReactHookForm';
-import { createMockedReportFormDetails } from '@/mocks/mockReport';
 
-describe('ReportForm', () => {
+describe('<ReportForm />', () => {
   // Mock 3rd party libraries
-  mockReactHookForm();
+  const { mockFormState, resetFormStateToDefault } = mockReactHookForm();
   mockMapbox({
     centerCoordinate: ALBRITTON_BELL_TOWER
   });
 
-  // Mock api call to fetch reports in the CodesignDataProvider
-  const reportData = [
-    createMockedReportFormDetails({ id: 1 }),
-    createMockedReportFormDetails({ id: 2 }),
-    createMockedReportFormDetails({ id: 3 })
-  ];
-  mockGetReportByBoundarySuccess({ reportData });
+  // getReportByBoundary to return mocked report data in CodesignDataProvider
+  mockGetReportByBoundarySuccess();
 
-  // Avoid sharing mock state betweeen tests
   afterEach(() => {
+    // Avoid sharing mock state betweeen tests
     jest.clearAllMocks();
+
+    // Reset report form's state between tests
+    resetFormStateToDefault();
   });
 
   const { ReportForm } = require('@/components/report/ReportForm');
@@ -109,6 +106,91 @@ describe('ReportForm', () => {
     });
     await waitFor(() => {
       expect(screen.getByText('Submit')).toBeVisible();
+    });
+  });
+
+  test('form should show validation error messages', async () => {
+    // Setup form state to have errors
+    const mockedFormErrors = {
+      reportLocation: {
+        message: 'Location is required',
+        type: 'required'
+      },
+      coordinates: {
+        message: 'Coordinates are required',
+        type: 'required'
+      },
+      reportLocationDetails: {
+        indoorDetails: {
+          buildingName: {
+            message: 'Building Name is required',
+            type: 'required'
+          },
+          floorNumber: {
+            message: 'Floor Number is required',
+            type: 'required'
+          }
+        }
+      },
+      images: {
+        message: 'Images are required',
+        type: 'required'
+      },
+      title: {
+        message: 'Title is required',
+        type: 'required'
+      },
+      description: {
+        message: 'Description is required',
+        type: 'required'
+      }
+    };
+    mockFormState({ errors: mockedFormErrors });
+
+    // When Report Form renders
+    render(
+      <TestContext>
+        <ReportForm />
+      </TestContext>
+    );
+
+    // Then form should show validation error messages
+    await waitFor(() => {
+      expect(
+        screen.getByText(mockedFormErrors.reportLocation.message)
+      ).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          mockedFormErrors.reportLocationDetails.indoorDetails.buildingName
+            .message
+        )
+      ).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText(
+          mockedFormErrors.reportLocationDetails.indoorDetails.floorNumber
+            .message
+        )
+      ).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText(mockedFormErrors.coordinates.message)
+      ).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(screen.getByText(mockedFormErrors.images.message)).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(screen.getByText(mockedFormErrors.title.message)).toBeVisible();
+    });
+    await waitFor(() => {
+      expect(
+        screen.getByText(mockedFormErrors.description.message)
+      ).toBeVisible();
     });
   });
 });

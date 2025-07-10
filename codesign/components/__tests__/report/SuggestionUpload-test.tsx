a9ed238efc25f3469bf3b90ce636108b50ba8822
malika-k <mkrkodial@gmail.com>
2025-07-10 15:47:57 -0500

    Fix failing test by adding provider component with setup logic

---
+++ codesign/components/__tests__/report/SuggestionUpload-test.tsx
@@ -0,0 +1,26 @@
+import { render, screen } from '@testing-library/react-native';
+
+import { SuggestionUpload } from '@/components/report/SuggestionUpload';
+import { ModalProvider } from '@/components/provider/ModalProvider';
+
+describe('<SuggestionUpload />', () => {
+  test('renders suggestion upload with message and button', () => {
+    // When view the Suggestion Upload component
+    render(
+      <ModalProvider>
+        <SuggestionUpload onChange={jest.fn()} value={''} />
+      </ModalProvider>
+    );
+
+    // Then show the suggestion message
+    expect(
+      screen.getByText('Would you like to suggest an improvement?')
+    ).toBeVisible();
+
+    // and show the SUGGEST button
+    expect(screen.getByText('SUGGEST')).toBeVisible();
+
+    // and show the sparkles image
+    expect(screen.getByTestId('suggestion-sparkles-image')).toBeVisible();
+  });
+});
